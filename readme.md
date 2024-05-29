24c628fc809477848597d75a092b43631b1f1b92
Rui Zhou <rui.zhou@sap.com>
2024-05-29 13:36:23 +0800

    update

---
+++ readme.md
@@ -0,0 +1,3 @@
+
+Swagger 2
+http://localhost:8080/swagger-ui.html
---
+++ src/main/java/org/example/VO/SwaggerResVO.java
@@ -0,0 +1,25 @@
+package org.example.VO;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.models.auth.In;
+import lombok.Data;
+
+
+@ApiModel("swagger response")
+@Data
+public class SwaggerResVO
+{
+
+	@ApiModelProperty("id")
+	private Integer id;
+
+	public void setId(Integer id){
+		this.id = id;
+	}
+
+	public Integer getId(){
+		return id;
+	}
+
+}
